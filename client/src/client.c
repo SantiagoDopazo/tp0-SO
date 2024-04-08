638d5765c0f319fe655d7055aa6f94a69261b1d9
Santiago Dopazo <dopazosantiago18@gmail.com>
2024-04-08 09:33:23 +0000

    logger creado y lee de archivo config

--- client/src/client.c
+++ client/src/client.c
@@ -18,7 +18,7 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	log_info(logger,"Hola! Soy un log");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -27,7 +27,13 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
+
 	// Loggeamos el valor de config
+
+	log_info(logger, "Lei la IP %s y puerto %s de la clave %s\n", ip, puerto, valor);
 
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
@@ -56,6 +62,10 @@ t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
 
+	if((nuevo_logger = log_create("tp0.log","TP0",1,LOG_LEVEL_INFO)) == NULL){
+		printf("El logger no se puedo crear");
+		exit(1);
+	}
 	return nuevo_logger;
 }
 
@@ -63,6 +73,10 @@ t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
 
+	if((nuevo_config = config_create("cliente.config")) == NULL){
+		printf("El config no se puedo crear");
+		exit(1);
+	}
 	return nuevo_config;
 }
 
@@ -97,4 +111,11 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	if(logger){
+		log_destroy(logger);
+	}
+	if(config){
+		config_destroy(config);
+	}
+	return;
 }
